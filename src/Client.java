22c146550892ad108f5dd207ebf7546ad90f99cd
Colin <cbrennan1@hawk.iit.edu>
2020-02-17 16:24:14 -0600

    Update Client.java
    Add Comments

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,7 @@
 /*Colin Brennan*/
 
 public abstract class Client {
-    public abstract void readData();
+    public abstract void readData();  //Read File Detail
+    abstract public void processData();  //Proccess File Detail
+	abstract public void printData();  //Print File Detail
 }
