3eaef655813c2d7bf1723eeac51b1237a7b64cb6
Colin <cbrennan1@hawk.iit.edu>
2020-02-17 16:55:04 -0600

    Create BankRecordsTest.java

---
+++ src/BankRecordsTest.java
@@ -0,0 +1,12 @@
+/*Colin Brennan ITM 411 Lab 2*/
+public class BankRecordsTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BankRecords Bank = new BankRecords ();
+		Bank.readData();
+
+		
+	}
+
+}
